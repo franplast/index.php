cc833e041e80d0159e53a8c3162c01b7c54423d5
Алексей <44683547+franplast@users.noreply.github.com>
2020-04-10 16:34:27 +0300

    Update beforeStepProps.php

--- modules/zvezda.importproductsxml/tools/beforeStepProps.php
+++ modules/zvezda.importproductsxml/tools/beforeStepProps.php
@@ -2,20 +2,27 @@
 include_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 use Bitrix\Main\Loader;
 Loader::includeModule("iblock");
-
+$status = "1"; // Пока доступен файл или нет, позже сюда же можно добавить обновлён удалённый или нет. 0- недоступен, 1 - доступен
+$message =""; // Заполняем, если есть что сказать, например, что файл не обновлялся с последней проверки, или что он не доступен
 $IBLOCK_TYPE = 'catalog'; // TODO get saved option
 $IBLOCK_IDS = $_POST['iblock_ids']; // TODO get saved option
-
-$arResult = [];
-$obj = new CIBlock;
-$rs = $obj->getList( [], ["TYPE" => $IBLOCK_TYPE, "ACTIVE" => "Y", "ID" => array_unique($IBLOCK_IDS)] );
-
-while($rsItem = $rs->Fetch()) {
-    $arResult['ITEMS'][] = [
-        "NAME" => $rsItem['NAME'],
-        "ID" => $rsItem['ID']
-    ];
+if(!$IBLOCK_IDS){
+    $status = 0;
 }
+else {
+    $arResult = [];
+    $obj = new CIBlock;
+    $rs = $obj->getList([], ["TYPE" => $IBLOCK_TYPE, "ACTIVE" => "Y", "ID" => array_unique($IBLOCK_IDS)]);
+
+    while ($rsItem = $rs->Fetch()) {
+        $arResult['ITEMS'][] = [
+            "NAME" => $rsItem['NAME'],
+            "ID" => $rsItem['ID']
+        ];
+    }
+}
+$arResult['STATUS'] = $status;
+$arResult['MASSAGE'] = $message;
 
 
 echo json_encode($arResult);
